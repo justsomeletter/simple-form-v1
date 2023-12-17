15e922cda2c331d8767f994c0893298fe0b46c20
justsomeletter <rahulchauhan2060@gmail.com>
2023-12-18 00:01:38 +0530

    gpt code

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,31 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+/*
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const form = e.target;
+  const formData = new FormData(form);
+
+  try {
+    const response = await fetch('http://localhost:5000/submit-form', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(Object.fromEntries(formData.entries())),
+    });
+
+    if (response.ok) {
+      console.log('Form submitted successfully');
+    } else {
+      console.error('Failed to submit form');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+*/
